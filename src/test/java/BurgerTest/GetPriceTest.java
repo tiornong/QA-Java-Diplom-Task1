fef31fbb273d7e4e1388a5112802094028271988
Bogdanov-Tsabert Timogor <mr.timogor@mail.ru>
2025-02-25 15:02:56 +0500

    Finished hard tests

--- src/test/java/BurgerTest/GetPriceTest.java
+++ src/test/java/BurgerTest/GetPriceTest.java
@@ -1,4 +1,31 @@
 package BurgerTest;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+
 public class GetPriceTest {
+
+    @Test
+    public void  getPriceTest() {
+        Bun bun = Mockito.mock(Bun.class);
+        Mockito.when(bun.getPrice()).thenReturn(100f);
+
+        Ingredient ingredient1 = Mockito.mock(Ingredient.class);
+        Mockito.when(ingredient1.getPrice()).thenReturn(50f);
+
+        Ingredient ingredient2 = Mockito.mock(Ingredient.class);
+        Mockito.when(ingredient2.getPrice()).thenReturn(200f);
+
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient1);
+        burger.addIngredient(ingredient2);
+
+        float expectedPrice = 100 * 2 + 50 + 200;
+        Assert.assertEquals(expectedPrice, burger.getPrice(), 0);
+    }
 }
--- src/test/java/BurgerTest/GetReceiptTest.java
+++ src/test/java/BurgerTest/GetReceiptTest.java
@@ -1,4 +1,53 @@
 package BurgerTest;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
 public class GetReceiptTest {
+
+    @Test
+    public void getReceiptShouldContainCorrectInformation() {
+        Bun bun = Mockito.mock(Bun.class);
+        Ingredient ingredient1 = Mockito.mock(Ingredient.class);
+
+        Mockito.when(bun.getName()).thenReturn("white bun");
+        Mockito.when(bun.getPrice()).thenReturn(200f);
+
+        Mockito.when(ingredient1.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(ingredient1.getName()).thenReturn("chili sauce");
+
+        Burger burger = new Burger();
+
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient1);
+
+        String expectedReceipt = "(==== white bun ====)\n" +
+                "= sauce chili sauce =\n" +
+                "(==== white bun ====)\n\n" +
+                "Price: 400,000000\n";
+
+        Assert.assertEquals(expectedReceipt, burger.getReceipt());
+    }
+
+    @Test
+    public void getReceiptWithNoIngredientsShouldShowOnlyBuns() {
+        Bun bun = Mockito.mock(Bun.class);
+        Mockito.when(bun.getName()).thenReturn("black bun");
+        Mockito.when(bun.getPrice()).thenReturn(150f);
+
+
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+
+        String expectedReceipt = "(==== black bun ====)\n" +
+                "(==== black bun ====)\n\n" +
+                "Price: 300,000000\n";
+
+        Assert.assertEquals(expectedReceipt, burger.getReceipt());
+    }
 }
